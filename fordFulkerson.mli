fb20a04fa1b660009719d844f82d23e42954fa4e
Ptiteigne <j-640@hotmail.fr>
2018-11-16 13:57:23 +0100

    Fix calc_max_flow type

--- fordFulkerson.mli
+++ fordFulkerson.mli
@@ -5,4 +5,4 @@ val string_of_flow_arc : flow_arc -> string
 
 val run_ff : flow_arc graph -> flow_arc graph
 val init_flow_graph : int graph -> flow_arc graph
-val calc_max_flow : 'a graph -> int
+val calc_max_flow : flow_arc graph -> int
