d460e37636661c68e0ece534c2fef2192d7b5a2d
M. Bonnasserre Julien <jbonnass@insa-08134>
2018-11-16 14:37:26 +0100

    Correct fonctions arguments

--- fordFulkerson.mli
+++ fordFulkerson.mli
@@ -3,6 +3,11 @@ type flow_arc = {capacity: int; flow: int}
 
 val string_of_flow_arc : flow_arc -> string
 
-val run_ff : flow_arc graph -> flow_arc graph
+(* Take a graph, a source node, a destination node
+ * then return the corresponding graph with the maximum flow *)
+val run_ff : flow_arc graph -> id -> id -> flow_arc graph
+
 val init_flow_graph : int graph -> flow_arc graph
-val calc_max_flow : flow_arc graph -> int
+
+(* Take a graph and its origin then return the maximum flow *)
+val calc_max_flow : flow_arc graph -> id -> int
