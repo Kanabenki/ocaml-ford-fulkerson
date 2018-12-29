27fe8683a4356e3f5e828749642bf85b7bc9d718
Ptiteigne <j-640@hotmail.fr>
2018-12-29 20:15:54 +0100

    Fix gap_graph building (confusion with the arcs direction)

--- fordFulkerson.ml
+++ fordFulkerson.ml
@@ -7,13 +7,13 @@ let string_of_flow_arc {capacity = c; flow = f} = Printf.sprintf "%d/%d" f c
 let init_flow_graph graph = Graph.map graph (fun c -> {capacity = c; flow = 0})
 
 let build_gap_graph graph =
-    let map_graph = Graph.map graph (fun data -> data.flow) in
+    let map_graph = Graph.map graph (fun data -> (data.capacity - data.flow)) in
     let graph_ids = Graph.get_nodes_list graph in
     let rec build_reverse gap_graph graph_ids = match graph_ids with
         | [] -> gap_graph
         | id :: rest -> (let rec iter_arcs gap_graph arcs = match arcs with
                 | [] -> gap_graph
-                | (aid, adata) :: rest -> iter_arcs (Graph.add_arc gap_graph aid id (adata.capacity - adata.flow)) rest
+                | (aid, adata) :: rest -> iter_arcs (Graph.add_arc gap_graph aid id adata.flow) rest
         in
         build_reverse (iter_arcs gap_graph (Graph.out_arcs graph id)) rest)
     in
