1a4ff6177c2a4a9ecc09f9dfb4af65cbd1813442
Ptiteigne <j-640@hotmail.fr>
2019-01-10 19:08:47 +0100

    Fix command arguments

--- ftestRoads.ml
+++ ftestRoads.ml
@@ -3,18 +3,15 @@ open FordFulkerson
 
 let () =
 
-  if Array.length Sys.argv <> 5 then
+  if Array.length Sys.argv <> 3 then
     begin
-      Printf.printf "\nUsage: %s infile source sink outfile\n\n%!" Sys.argv.(0) ;
+      Printf.printf "\nUsage: %s infile outfile\n\n%!" Sys.argv.(0) ;
       exit 0
     end ;
 
   let infile = Sys.argv.(1)
-  and outfile = Sys.argv.(4)
+  and outfile = Sys.argv.(2)
   
-  (* These command-line arguments are not used for the moment. *)
-  and _source = Sys.argv.(2)
-  and _sink = Sys.argv.(3)
   in
 
   (* Open file *)
